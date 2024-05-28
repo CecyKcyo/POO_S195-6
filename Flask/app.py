1ce368a6d57535fb7bf9fa9b7429e4276804e7c0
CecyKcyo <122041716@upq.edu.mx>
2024-05-27 20:12:54 -0600

    Rutas con Flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,8 +1,44 @@
+from flask import Flask,Request
+
 #importamos la clase Flask desde el modulo flask
 from flask import Flask
 
 #creamos una instancia de la clase Flask, esta se convierte en la aplicación web
 app = Flask(__name__)
+
+#Ruta simple
+@app.route('/')
+def principal():
+    return 'Hola mundo Flask'
+
+
+#Ruta doble 
+@app.route('/usuario')
+@app.route('/saludar')
+def saludo():
+    return 'Hola, Ivan Isay'
+
+#Rutas con parametros 
+@app.route('/hi/<nombre>')
+def hi(nombre):
+    return 'Hola '+nombre+'!!!'
+
+#Definicion de metodos de trabajo
+@app.route('/formulario/', methods=['GET', 'POST'])
+def formulario():
+    if Request.method == 'GET':
+        return 'No es seguro mandar password por GET, no seas tibio'
+    elif Request.method == 'POST':
+        return 'SI es seguro mandar password por POST'
+    
+#Manejador de exepciones 
+@app.errorhandler(404)
+def paginano(e):
+    return'Revisa tu sintaxis, no encontre nada'
+    
+
+    
+
 
 #condiconamos para verificar si el script se está ejecutando directamente (no importado como módulo)
 if __name__ == '__main__':
